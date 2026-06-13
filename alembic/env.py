9344aaa632b69445a987d215c147d6bedb6b9ad1
Hasee <fabiob@list.ru>
2026-06-13 19:04:28 +0300

    Add Docker setup for API and database

--- alembic/env.py
+++ alembic/env.py
@@ -1,4 +1,6 @@
+import os
 from logging.config import fileConfig
+
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -8,6 +10,13 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+database_url = os.getenv("DATABASE_URL")
+
+if database_url:
+    config.set_main_option(
+        "sqlalchemy.url",
+        database_url.replace("+asyncpg", "+psycopg"),
+    )
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
